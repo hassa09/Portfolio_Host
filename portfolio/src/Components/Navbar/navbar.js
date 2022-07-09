797992418db12d0f2fa996bc1c46efeec72c8f4a
Your Name <you@example.com>
2022-07-09 20:48:23 +0100

    navbar with link elements created and working however functionality is in progress

---
+++ portfolio/src/Components/Navbar/navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Close, MenuOutlined, } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
+import './navbar.scss';
+
+function Navbar() { 
+    const [active, setActive] = useState(false)
+    //Navbar icon functionality function 
+    const DisplayMenu = () => {
+        setActive(!active)
+    }
+    return (
+        <div className='header'>
+            <div className='navlogo'>
+                <h1>HA</h1>
+            </div>
+            <nav className={active ? 'navbar':'navbar'}>
+                <ul>
+                    <div className='closed'>
+                        <Close className='closed' onClick={DisplayMenu}/>
+                    </div>
+
+                    <li>
+                        <Link to='/'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Skills</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Contact</Link>
+                    </li>
+                    <li>
+                        <button to='/'>CV</button>
+                    </li>
+                    
+                </ul>
+            </nav>
+             {/* handed display function as prop for menu icon functionanility when it's used on small devices eg mobile devices*/}
+            <div className='menubar'>
+                <MenuOutlined className='menu' onClick={DisplayMenu} />
+            </div>
+        </div>
+
+    )
+}
+export default Navbar
