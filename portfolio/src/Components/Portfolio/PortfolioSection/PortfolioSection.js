5ca23b2f105f7b4ad71cae98cc392979e1edae90
leoarthurwall <leowall93@gmail.com>
2022-07-11 10:12:13 +0100

    portfolio section layout and functionality done

--- portfolio/src/Components/Portfolio/PortfolioSection/PortfolioSection.js
+++ portfolio/src/Components/Portfolio/PortfolioSection/PortfolioSection.js
@@ -1,23 +1,23 @@
-import PortfolioIndividual from "../PortfolioIndividual/PortfolioIndividual";
-import "./PortfolioSection.css"
+import "../portfolio.css";
 import Tab from "../TabComponent/Tab";
+import DesignTab from "../DesignTab/DesignTab";
 
 function PortfolioSection () {
     return (
         <div className="portfolio-section-container">
             <div className="portfolio-section-banner"> 
-                <div className="portfolio-section-header">Header</div>
+                <div className="portfolio-section-header">Portfolio</div>
                 <div className="portfolio-section-tab">
-                    <Tab />
+                    {/* <Tab /> */}
                 </div>
             </div>
             <div className="portfolio-section-main"> 
-                <PortfolioIndividual />
-                <PortfolioIndividual />
-                <PortfolioIndividual />
+                <Tab />
             </div>
         </div>
     );
 }
 
 export default PortfolioSection
+
+// TABS TUTORIAL: https://blog.logrocket.com/how-to-build-tab-component-react/
--- portfolio/src/Components/Portfolio/TabComponent/Tab.js
+++ portfolio/src/Components/Portfolio/TabComponent/Tab.js
@@ -1,14 +1,41 @@
-import React from "react";
+import DevelopmentTab from "../DevelopmentTab/DevelopmentTab";
+import DesignTab from "../DesignTab/DesignTab";
+import { useState } from "react";
+
 const Tab = () => {
+    const [activeTab, setActiveTab] = useState("tab1");
+
+
+//  Functions to handle Tab Switching
+const handleTab1 = () => {
+    // update the state to tab1
+    setActiveTab("tab1");
+  };
+  const handleTab2 = () => {
+    // update the state to tab2
+    setActiveTab("tab2");
+  };
+
   return (
     <div className="Tabs">
       {/* Tab nav */}
       <ul className="nav">
-        <li>Tab 1</li>
-        <li>Tab 2</li>
-      </ul>
+  <li
+    className={activeTab === "tab1" ? "active" : ""}
+    onClick={handleTab1}
+  >
+    Design
+  </li>
+  <li
+    className={activeTab === "tab2" ? "active" : ""}
+    onClick={handleTab2}
+  >
+    Develop
+  </li>
+</ul>
       <div className="outlet">
-        {/* content will be shown here */}
+          {activeTab === "tab1" ? <DesignTab /> : <DevelopmentTab />}
+    
       </div>
     </div>
   );
