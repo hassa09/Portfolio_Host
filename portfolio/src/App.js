dab149c3cb85dbd002cbe17a7653c8cba166a86b
leoarthurwall <leowall93@gmail.com>
2022-07-12 12:28:53 +0100

    about section complete

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,15 @@ import About from './Components/About/about'
 import './App.css';
 import PortfolioSection from './Components/Portfolio/PortfolioSection/PortfolioSection';
 import Contact from './Components/Contact/Contact';
+import Intro from './Components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
       <About />
+      <Intro />
       <PortfolioSection />
       <Contact />
     </div>
