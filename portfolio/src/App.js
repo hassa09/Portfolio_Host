630212ef05981fd7bc79fac9f48789de8aac4f52
leoarthurwall <leowall93@gmail.com>
2022-07-12 14:08:39 +0100

    changed about and intro names around to be correct order

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,16 @@
 import Navbar from './Components/Navbar/navbar';
-import About from './Components/About/about'
 import './App.css';
 import PortfolioSection from './Components/Portfolio/PortfolioSection/PortfolioSection';
 import Contact from './Components/Contact/Contact';
-import Intro from './Components/Intro/Intro';
+import Introduction from './Components/Introduction/Introduction'
+import About from './Components/About/About'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Introduction />
       <About />
-      <Intro />
       <PortfolioSection />
       <Contact />
     </div>
--- portfolio/src/Components/About/about.js
+++ portfolio/src/Components/About/about.js
@@ -1,37 +1,30 @@
-import { GitHub, LinkedIn, Twitter } from '@material-ui/icons';
+import './About.css'
 
-import React from 'react';
+function About () {
 
-// import Navbar from '../Navbar/navbar';
-import './about.scss';
-
-function about() {
     return (
-      <div main className='about'>
-            <div className='aboutContainer'>
-
-
-                <div mainCotent>
-                    <div className='text'>
-              <p>HI, my name is</p>
-              <h1>'your name here.'</h1>
-              <h2>i design & develope things.</h2>
-              <p> i am full stack developer & Cyber security analyst. i've just completed the prestigious School of code,looking to enter the world of software developement.</p>
-
-              <button className='getInT'>get in touch </button>
-              <div className='icons'>
-                  <GitHub className='icon' />
-                  <LinkedIn className='icon'  />
-                  <Twitter className='icon' />
-                  
-             </div>
-              </div>
-              
+        <div className="about-section-container">
+            <div className="about-section-header">
+                <h1 className="about-section-header-wording">About<em>.</em></h1>
             </div>
-          
-        </div>
-      </div>
-  )
-}
+            <div className="about-section-main"> 
+                <div className="about-section-left">
+                    <text className='about-section-summary'>
+                        <p className='about-section-summary-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae  consequuntur voluptatum laborum numquam </p> 
+                        <p className='about-section-summary-p' >blanditiis harum quisquam eius sed odit fugiat usto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut </p>
+                        <p className='about-section-summary-p' >molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit</p>
+                        <p className='about-section-summary-p' >Here are a few technologies I work with:</p>
+                        <p className='about-section-tech-icons'>Insert Skills Here</p>
+                    </text>
+                    
 
-export default about
+                </div>
+                <div className="about-section-right">
+                    <img className="about-section-profile-photo" src={require('/Users/leowall/Documents/SchoolOfCode/Portfolio/portfolioWebPage/Portfolio_Host/portfolio/src/Images/pngegg.png')} alt={"profile picture image"}/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default About
